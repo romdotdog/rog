f7512e822eebed6ab1df52fa9f98486386302309
romdotdog <70765447+romdotdog@users.noreply.github.com>
2024-08-05 13:01:42 -0400

    fix <code> bug

--- rog/src/Markdown/index.tsx
+++ rog/src/Markdown/index.tsx
@@ -1,14 +1,15 @@
 import type { Component } from "solid-js";
 import { parse } from "./Parser";
 import { marked } from "marked";
+import style from "./Markdown.module.css";
 import markedKatex from "marked-katex-extension";
 
 // Thanks to https://github.com/sibiraj-s/marked-react for the base code
 
 marked.use(markedKatex());
 
-const Markdown: Component<{ value: string }> = (props) => {
-    return <>{parse(marked.lexer(props.value))}</>;
+const Markdown: Component<{ value: string }> = props => {
+    return <div style={style.markdown}>{parse(marked.lexer(props.value))}</div>;
 };
 
 export default Markdown;
